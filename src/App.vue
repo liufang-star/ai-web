4119c7b5467dc3e5315abf8c11849c5e46b5b59a
刘芳 <253199468@qq.com>
2023-04-21 18:36:59 +0800

    第二次修改

--- src/App.vue
+++ src/App.vue
@@ -167,7 +167,7 @@
                       class="text-gray-800 w-full md:max-w-2xl lg:max-w-3xl md:h-full md:flex md:flex-col px-6 dark:text-gray-100">
                       <h1
                         class="text-4xl font-semibold text-center mt-6 sm:mt-[20vh] ml-auto mr-auto mb-10 sm:mb-16 flex gap-2 items-center justify-center">
-                        百济智能问答</h1>
+                        ChatGPT</h1>
                       <div class="md:flex items-start text-center gap-3.5">
                         <div class="flex flex-col mb-8 md:mb-auto gap-3.5 flex-1">
                           <h2 class="flex gap-3 items-center m-auto text-lg font-normal md:flex-col md:gap-2">
@@ -184,20 +184,24 @@
                               <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
                               <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
                             </svg>
-                            示例
+                            Examples
                           </h2>
                           <ul class="flex flex-col gap-3.5 w-full sm:max-w-md m-auto">
-                            <button @click="inputChat('百济神州有哪些产品')"
+                            <button @click="inputChat('Explain quantum computing in simple terms')"
                               class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md hover:bg-gray-200 dark:hover:bg-gray-900">
-                              "百济神州有哪些产品" →
+                              "Explain
+                              quantum computing in simple terms" →
                             </button>
-                            <button @click="inputChat('百济神州的研发能力如何')"
+                            <button @click="inputChat('Got any creative ideas for a 10 year old’s birthday?')"
                               class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md hover:bg-gray-200 dark:hover:bg-gray-900">
-                              "百济神州的研发能力如何" →
+                              "Got
+                              any creative ideas for a 10 year old’s birthday?"
+                              →
                             </button>
-                            <button @click="inputChat('百济神州的价值观')"
+                            <button @click="inputChat('How do I make an HTTP request in Javascript?')"
                               class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md hover:bg-gray-200 dark:hover:bg-gray-900">
-                              "百济神州的价值观" →
+                              "How
+                              do I make an HTTP request in Javascript?" →
                             </button>
                           </ul>
                         </div>
@@ -209,15 +213,17 @@
                                 d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z">
                               </path>
                             </svg>
-                            能力
+                            Capabilities
                           </h2>
                           <ul class="flex flex-col gap-3.5 w-full sm:max-w-md m-auto">
                             <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">
-                              记住用户之前在对话中所说的话
+                              Remembers what user said earlier in the conversation
                             </li>
-                            <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">允许用户提供后续更正
+                            <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">Allows
+                              user to provide follow-up corrections
                             </li>
-                            <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">接受过拒绝不当请求的培训
+                            <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">Trained
+                              to decline inappropriate requests
                             </li>
                           </ul>
                         </div>
@@ -232,14 +238,17 @@
                               <line x1="12" y1="9" x2="12" y2="13"></line>
                               <line x1="12" y1="17" x2="12.01" y2="17"></line>
                             </svg>
-                            局限性
+                            Limitations
                           </h2>
                           <ul class="flex flex-col gap-3.5 w-full sm:max-w-md m-auto">
-                            <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">可能偶尔生成不正确的信息
+                            <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">May
+                              occasionally generate incorrect information
                             </li>
-                            <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">可能偶尔产生有偏见的内容
+                            <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">May
+                              occasionally produce harmful instructions or biased content
                             </li>
-                            <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">对2021年后的事件了解有限
+                            <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">Limited
+                              knowledge of world and events after 2021
                             </li>
                           </ul>
                         </div>
@@ -280,7 +289,7 @@
                         <polyline points="23 20 23 14 17 14"></polyline>
                         <path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15"></path>
                       </svg>
-                      <p class="none">重新生成响应</p>
+                      <p class="none">Regenerate response</p>
                     </button>
 
                     <button v-if="convLoading" @click.stop.prevent="stopChat" id="stopChat"
@@ -290,7 +299,7 @@
                           stroke-linecap="round" stroke-linejoin="round" class="h-3 w-3" height="1em" width="1em"
                           xmlns="http://www.w3.org/2000/svg">
                           <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
-                        </svg>停止生成
+                        </svg>Stop generating
                       </div>
                     </button>
 
@@ -317,7 +326,7 @@
               </form>
               <div class="px-3 pt-2 pb-3 text-center text-xs text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
                 <a href="https://gitee.com/MIEAPP/chatai-vue" target="_blank" rel="noreferrer"
-                  class="underline"></a>
+                  class="underline">开源：chatai-vue</a> 本开源项目前端使用vue，后端基于openai开放api开发，仅供学习 AI 使用。
               </div>
             </div>
           </main>
@@ -336,7 +345,7 @@
                     <line x1="12" y1="5" x2="12" y2="19"></line>
                     <line x1="5" y1="12" x2="19" y2="12"></line>
                   </svg>
-                  新聊天
+                  New chat
                 </a>
 
                 <!-- 对话列表 -->
@@ -383,7 +392,7 @@
                           <line x1="10" y1="11" x2="10" y2="17"></line>
                           <line x1="14" y1="11" x2="14" y2="17"></line>
                         </svg>
-                        <div class="flex-1 text-ellipsis max-h-5 overflow-hidden break-all relative">删除 "{{
+                        <div class="flex-1 text-ellipsis max-h-5 overflow-hidden break-all relative">Delete "{{
                           conversation.title
                         }}"?
                           <div class="absolute inset-y-0 right-0 w-8 z-10 bg-gradient-to-l from-gray-800"></div>
@@ -462,7 +471,7 @@
                     <line x1="10" y1="11" x2="10" y2="17"></line>
                     <line x1="14" y1="11" x2="14" y2="17"></line>
                   </svg>
-                  清除对话
+                  Clear conversations
                 </a>
                 <a v-if="theme == 'light'" @click="changeTheme('dark')"
                   class="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm">
@@ -487,7 +496,7 @@
                     <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
                     <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
                   </svg>
-                  灯光模式</a>
+                  Light mode</a>
 
                 <a href="https://discord.gg/openai" target="_blank"
                   class="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm">
@@ -497,7 +506,7 @@
                       d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z">
                     </path>
                   </svg>
-                  OpenAI 社区</a>
+                  OpenAI Discord</a>
                 <a href="https://help.openai.com/en/collections/3742473-chatgpt" target="_blank"
                   class="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm">
                   <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round"
@@ -506,7 +515,7 @@
                     <polyline points="15 3 21 3 21 9"></polyline>
                     <line x1="10" y1="14" x2="21" y2="3"></line>
                   </svg>
-                  更新和常见问题解答</a>
+                  Updates &amp; FAQ</a>
               </nav>
 
             </div>
@@ -587,7 +596,7 @@
                     </div>
                     <div class="w-full h-[1px] bg-gray-300 opacity-20"></div>
                     <h4 class="mb-4">
-                      项目基于openai开放api开发，使用的是davinci模型.</h4>
+                      本开源项目基于openai开放api开发，使用最新的gpt-3.5-turbo，前端使用vue高仿ChatGpt界面，仅供学习使用.</h4>
                     <div class="flex gap-4 flex-col text-sm">
                       <div class="flex p-4 bg-gray-50 dark:bg-white/5 rounded-md items-center gap-4 min-h-[71px]">
                         <div class="w-10 text-2xl text-center">🚨</div>
@@ -602,7 +611,7 @@
                       </div>
                     </div>
                     <div class="flex gap-4 mt-6">
-                      <button @click="closePopup" class="btn flex justify-center gap-2 btn-primary ml-auto">关闭
+                      <button @click="closePopup" class="btn flex justify-center gap-2 btn-primary ml-auto">Done
                       </button>
                     </div>
                   </div>
